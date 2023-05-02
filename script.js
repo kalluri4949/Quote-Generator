ce492b496cc2ea881496f3cab8ea100f6c532b56
Rakesh Kalluri <kalluri.rakesh15@gmail.com>
2023-05-02 09:46:35 -0400

    Added Loader

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.querySelector('.loader');
 // Get Quotes From API
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new Quote
 function newRandomQuote() {
+  loading();
   const random = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[random];
   const { author, text } = quote;
-  //  Check if Autho is blank and replace it with unknown
+  //  Check if Author is blank and replace it with unknown
   if (!author) {
     authorText.textContent = 'Unknown';
   } else {
@@ -24,11 +38,14 @@ function newRandomQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set Quote, Hide Loader
   quoteText.textContent = text;
+  complete();
   console.log(quote);
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const response = await fetch(apiUrl);
